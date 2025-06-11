7aa407636519a94fba718b0e2d3c4071adc9863b
Alexey Vodoley <Alex-vodolei@rambler.ru>
2025-06-11 11:11:38 +0400

    refactor: delete

--- app/components/App/App.tsx
+++ app/components/App/App.tsx
@@ -15,33 +15,10 @@ const siderStyle: React.CSSProperties = {
     scrollbarGutter: 'stable',
 };
 
-// const items: MenuProps['items'] = [
-//     UserOutlined,
-//     VideoCameraOutlined,
-//     UploadOutlined,
-//     BarChartOutlined,
-//     CloudOutlined,
-//     AppstoreOutlined,
-//     TeamOutlined,
-//     ShopOutlined,
-// ].map((icon, index) => ({
-//     key: String(index + 1),
-//     // icon: React.createElement(icon),
-//     label: `nav ${index + 1}`,
-// }));
-
 const App: React.FC = () => {
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
-
-    // const add = async () => {
-    //     await setDoc(doc(db, "cities", "LA"), {
-    //         name: "Los Angeles",
-    //         state: "CA",
-    //         country: "USA"
-    //     });
-    // }
 
     return (
         <Layout hasSider>
--- app/root.tsx
+++ app/root.tsx
@@ -9,18 +9,18 @@ import {
 
 import type { Route } from "../.react-router/types/app/+types/root";
 
-export const links: Route.LinksFunction = () => [
-    { rel: "preconnect", href: "https://fonts.googleapis.com" },
-    {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossOrigin: "anonymous",
-    },
-    {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-    },
-];
+// export const links: Route.LinksFunction = () => [
+//     { rel: "preconnect", href: "https://fonts.googleapis.com" },
+//     {
+//         rel: "preconnect",
+//         href: "https://fonts.gstatic.com",
+//         crossOrigin: "anonymous",
+//     },
+//     {
+//         rel: "stylesheet",
+//         href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+//     },
+// ]; // Разобраться с типами
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
             <Meta />
             <Links />
         </head>
-        <body>
+        <body style={{margin: 0}}>
         {children}
         <ScrollRestoration />
         <Scripts />
